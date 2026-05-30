da665032768eccadc763e1ae6fcaeab9a590f326
Udayanga Jayasundara <udayanga@mac.lan>
2026-05-30 17:36:21 -0400

    Initial commit

---
+++ src/test/kotlin/com/udayanga/kotlin_demo/KotlinDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.udayanga.kotlin_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
